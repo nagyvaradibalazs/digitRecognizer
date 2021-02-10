3a728aacea3458d911bbfd915bddda0243beb830
Balazs Nagyvaradi <balazs.nagyvaradi@aalto.fi>
2021-02-10 23:18:17 +0100

    Drawing on touchscreen update

--- script/drawer.js
+++ script/drawer.js
@@ -3,13 +3,20 @@ var isDrawing;
 var x;
 var y;
 var ctx;
+var canvas;
 
-const initCanvas = (canvas, ctxOriginal) => {
+const updateXY = (e) => {
+	x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+	y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+}
+
+const initCanvas = (canvasOriginal, ctxOriginal) => {
+	canvas = canvasOriginal
 	ctx = ctxOriginal;
 	ctx.lineJoin = "round";
 	ctx.lineCap = "round";
 	ctx.lineWidth = 10;
-	ctx.strokeStyle = "red";
+	ctx.strokeStyle = "black";
 
 	isDrawing = false;
 	x = 0;
@@ -34,16 +41,9 @@ const initCanvas = (canvas, ctxOriginal) => {
 		}
 
 		isDrawing = true;
-		x = e.offsetX;
-		y = e.offsetY;
+		updateXY(e);
 	});
-	canvas.addEventListener("touchmove", (e) => {
-		if(e.target == canvas) {
-			e.preventDefault();
-		}
-
-		drawOnCanvas
-	});
+	canvas.addEventListener("touchmove", drawOnCanvasMobile);
 	canvas.addEventListener("touchend", () => isDrawing = false);
 }
 
@@ -57,6 +57,18 @@ const drawOnCanvas = (e) => {
 
 		x = e.offsetX;
 		y = e.offsetY;
+	}
+}
+
+const drawOnCanvasMobile = (e) => {
+	if(isDrawing) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(e.touches[0].clientX - canvas.getBoundingClientRect().left, e.touches[0].clientY - canvas.getBoundingClientRect().top);
+		ctx.closePath();
+		ctx.stroke();
+
+		updateXY(e);
 	}
 }
 
